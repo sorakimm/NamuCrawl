ed96bc8629ed363d996c3bbb0e2b29045d1fb8d5
Sorak <sorak17@gmail.com>
2018-08-09 19:07:02 +0900

    crawler class update

--- src/crawler.py
+++ src/crawler.py
@@ -7,107 +7,109 @@ from bs4 import BeautifulSoup
 import db
 
 
-
 crawlLogFile = "log/crawler.log"
 crawlLogger = MyLogger(crawlLogFile)
 baseUrl = "https://namu.wiki/w/"
 
-def get_html(url):
-    crawlLogger.debug('get_html : ' + url)
-    _html = ""
-    try:
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            _html = resp.text
 
-    except requests.exceptions.RequestException as e:
-        crawlLogger.error(e + "url : " + url)
-    return _html
+class Crawler():
+    def __init__(self):
+        self.url = ""
+        self.title = ""
+        self.image = ""
+        self.editdate = ""
+        self.content = ""
+        self.html = ""
+        self.bsObj = ""
+        self.dbTuple = tuple()
 
+    def getCrawl(self, _url):
+        try:
+            self.url = _url
+            crawlLogger.debug("getCrawl url : " + self.url)
 
-def getTitle(bsObj):
-    try:
-        title = bsObj.find("h1", {"class": "title"}).text.strip()
-        crawlLogger.debug('getTitle : ' + title)
-        return title
+            try:
+                resp = requests.get(self.url)
+                self.html = resp.text
+                self.bsObj = BeautifulSoup(self.html, 'html.parser')
 
-    except Exception as e:
-        crawlLogger.error(e + "title : " + title)
+                if resp.status_code == 200:
+                    self.getTitle()
+                    self.getImage()
+                    self.getEditDate()
+                    self.getContent()
 
+                if resp.status_code == 404:
+                    self.getTitle()
+                    self.image = None
+                    self.editdate = None
+                    self.content = None
 
-def getImage(bsObj):
-    try:
-        fullImageUrl = ""
-        imgPattern = re.compile('^(파일:)(?!나무위키).*?$')
-        for imageUrl in bsObj.findAll("img", {"data-src": re.compile('^(//cdn.namuwikiusercontent.com)(.*?)$')}):
-            if imgPattern.search(imageUrl.attrs['alt']):
-                fullImageUrl = 'https:' + imageUrl.attrs['data-src']
-                crawlLogger.debug('getImage : ' + fullImageUrl)
-                break
+            except requests.exceptions.RequestException as e:
+                crawlLogger.error(e + "url : " + self.url)
 
-        return fullImageUrl
+            self.dbTuple = (self.title, self.url, self.content, self.image, self.editdate, self.html, self.url)
+            db.insertNamuwikiDB(self.dbTuple)
 
-    except Exception as e:
-        crawlLogger.error(e)
-        return None
+            linkList = []
+            crawlLogger.debug(linkList)
+            for link in self.bsObj.findAll("a", href=re.compile("^(/w/)((?!:).)*?$")):
+                linkList.append(urljoin(baseUrl, link.get('href')))
 
+            return linkList
 
-def getEditDate(bsObj):
-    try:
-        editDate = bsObj.find("p", {"class": "wiki-edit-date"}).find("time").text
-        crawlLogger.debug('getEditDate : ' + editDate)
-        return editDate
-
-    except Exception as e:
-        crawlLogger.error(e + "editdate : " + editDate)
-        return None
-
-
-def getContent(bsObj):
-    try:
-        content = bsObj.find("div", {"class": "wiki-inner-content"}).get_text(" ")
-        content = re.sub(r'\s{2,}', ' ', content)
-        crawlLogger.debug('getContent')
-        return content
-
-    except Exception as e:
-        crawlLogger.error(e)
-        return None
+        except Exception as e:
+            crawlLogger.error(e)
 
 
-def getCrawl(pageUrl):
-    try:
-        crawlLogger.debug('getCrawl')
-        fullPageUrl = urljoin(baseUrl, pageUrl)
-        html = get_html(fullPageUrl)
-        bsObj = BeautifulSoup(html, 'html.parser')
+    def getTitle(self):
+        try:
+            self.title = self.bsObj.find("h1", {"class": "title"}).text.strip()
+            crawlLogger.debug('getTitle : ' + self.title)
 
-        if bsObj == None:
-            return None
-        title = getTitle(bsObj)
-        content = getContent(bsObj)
-        editDate = getEditDate(bsObj)
-        image = getImage(bsObj)
-        dbTuple = (title, fullPageUrl, content, image, editDate, html, fullPageUrl)
-        db.insertNamuwikiDB(dbTuple)
-
-        linkList = []
-        crawlLogger.debug(linkList)
-        for link in bsObj.findAll("a", href=re.compile("^(/w/)((?!:).)*?$")):
-            linkList.append(urljoin(baseUrl, link.get('href')))
-
-        return linkList
-
-    except Exception as e:
-        crawlLogger.error(dbTuple)
+        except Exception as e:
+            crawlLogger.error(e + "title : " + self.title)
 
 
-def getRecentChangeLink(url):
-    recentChangeLinkSet = set()
-    html = get_html(url)
-    bsObj = BeautifulSoup(html, 'html.parser')
-    for recentChangeLink in bsObj.find("div", {"id": "recentChangeTable"}).findAll("a"):
-        recentChangeLinkSet.add(recentChangeLink.get('href'))
+    def getImage(self):
+        try:
+            imgPattern = re.compile('^(파일:)(?!나무위키).*?$')
+            for imageUrl in self.bsObj.findAll("img", {"data-src": re.compile('^(//cdn.namuwikiusercontent.com)(.*?)$')}):
+                if imgPattern.search(imageUrl.attrs['alt']):
+                    self.imageUrl = 'https:' + imageUrl.attrs['data-src']
+                    crawlLogger.debug('getImage : ' + self.imageUrl)
+                    break
 
-    return recentChangeLinkSet
+        except Exception as e:
+            crawlLogger.error(e)
+
+
+    def getEditDate(self):
+        try:
+            self.editdate = self.bsObj.find("p", {"class": "wiki-edit-date"}).find("time").text
+            crawlLogger.debug('getEditDate : ' + self.editdate)
+
+        except Exception as e:
+            crawlLogger.error(e + "editdate : " + self.editdate)
+
+
+    def getContent(self):
+        try:
+            self.content = self.bsObj.find("div", {"class": "wiki-inner-content"}).get_text(" ")
+            self.content = re.sub(r'\s{2,}', ' ', self.content)
+            crawlLogger.debug('getContent')
+
+        except Exception as e:
+            crawlLogger.error(e)
+
+
+
+    # def getRecentChangeLink(url):
+    #     recentChangeLinkSet = set()
+    #     html = get_html(url)
+    #     bsObj = BeautifulSoup(html, 'html.parser')
+    #     for recentChangeLink in bsObj.find("div", {"id": "recentChangeTable"}).findAll("a"):
+    #         recentChangeLinkSet.add(recentChangeLink.get('href'))
+    #
+    #     return recentChangeLinkSet
 
